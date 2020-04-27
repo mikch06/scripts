0ff021c070f7cc486fe31cca952d9d06a6622abe
mikch06 <tools@kissel.ch>
2020-04-27 17:08:35 +0200

    Create deploy_website.sh

---
+++ deploy_website.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ "$1" = "" ]
+then
+    echo "> No deployment!"
+    echo "> Choose test, int, prod"
+elif [ "$1" = "test" ]
+then
+  echo "deployment on test environment!!"
+elif [ "$1" = "prod" ]
+then
+  echo "deployment on prod environment!!"
+  sleep 3
+  echo "build website data"
+  mkdocs build --clean
+  echo "transfer data to prod"
+  rsync -avz --delete site/ mikcyons@kissel.ch:/home/mikcyons/www/kissel.ch/wiki/
+else
+  echo ""
+fi
+
+echo ""
+echo "end of script"
