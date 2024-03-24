5cb762774eb7c7e72b0c7b33fa83b25337aeaa39
mik <tools@kissel.ch>
2024-03-24 13:27:20 +0100

    Create config.py

---
+++ conf/config.py
@@ -0,0 +1,10 @@
+## config
+
+# Mail settings
+sender_email = "admin@host.ch"
+receiver_email = "receiver@test.ch"
+subject = "The ultimate update mailer!"
+
+# Smtp settings
+smtp_user = "smtp@domain.ch"
+smtp_password = "smtp-password"
